235a673942a953cb0047182a1297f45752bafdab
Akshatraj077 <150254367+Akshatraj077@users.noreply.github.com>
2024-09-11 14:01:45 +0000

    day 1

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,3 @@
+"use strict" //treat all JS codes as newer version
+
+//alert(3+3) //will work in browser but not here
---
+++ 01_basics/variables.js
@@ -0,0 +1,15 @@
+const acc_id = 144553
+let acc_mail = "akshat@gmail.com"
+var acc_pass = "12345"
+acc_city = "kolkata"
+let acc_state
+
+// acc_id = 2
+acc_mail = "archita@gmail.com"
+acc_pass = "2121"
+acc_city = "bengaluru"
+
+console.log("acc_id");
+console.table([acc_id, acc_mail, acc_pass, acc_city, acc_state])
+
+/* never use var coz it has a scope problem in blocks and functions */
