41bf5e6f642123bb929130e2717b41b0e263ce52
Akshatraj077 <150254367+Akshatraj077@users.noreply.github.com>
2024-09-12 14:19:15 +0000

    day 2

---
+++ 01_basics/ConversionAndOperations.js
@@ -0,0 +1,41 @@
+let score = "33"
+
+console.log(typeof(score));
+
+let numbervalue = Number(score)
+
+console.log(typeof(numbervalue));
+
+
+let score1 = "33abs"
+
+console.log(typeof(score1));
+
+let numbervalue1 = Number(score1)
+
+console.log(typeof(numbervalue1));
+console.log(numbervalue1);
+
+
+let score2 = null
+
+console.log(typeof(score2));
+
+let numbervalue2 = Number(score2)
+
+console.log(typeof(numbervalue2));
+console.log(numbervalue2);
+
+
+let LetLoggedIn = 2
+let convert = Boolean(LetLoggedIn)
+
+console.log(typeof convert);
+console.log(convert);
+
+
+let LetLoggedIn1 = ""
+let convert1 = Boolean(LetLoggedIn1)
+
+console.log(typeof convert1);
+console.log(convert1);
