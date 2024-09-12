fd55373174d8d0da501c1bbff07b728a59a6a352
Akshatraj077 <150254367+Akshatraj077@users.noreply.github.com>
2024-09-12 14:03:46 +0000

    day 1

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -1,3 +1,18 @@
 "use strict" //treat all JS codes as newer version
 
 //alert(3+3) //will work in browser but not here
+
+let age = 18
+let isloggedin = false
+let state = null
+
+//number -> 2 to power 53
+//bigint
+//null = standalone value
+//undefined
+//symbol -> unique
+//object
+
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
