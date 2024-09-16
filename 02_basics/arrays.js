cae194570ea4c2aa24b2e836b4ac663d22a85277
Akshatraj077 <150254367+Akshatraj077@users.noreply.github.com>
2024-09-16 11:33:16 +0000

    day 4

---
+++ 02_basics/arrays.js
@@ -0,0 +1,56 @@
+const arr = [1, 1, 2, 5, 4]
+const arr1 = ["hi", "hello"]
+const arr2 = new Array(2,4,7,9)
+
+console.log(arr[2]);
+arr.push(3)
+console.log(arr);
+arr.pop()
+console.log(arr);
+
+arr.unshift(9)
+console.log(arr);
+arr.shift();
+console.log(arr);
+arr.shift();
+console.log(arr);
+
+console.log(arr.includes(9));
+console.log(arr.indexOf(9));
+
+// const newarr = [9, 8]
+// newarr = arr.join()
+// console.log(newarr);
+
+const temp = [1, 2, 3, 4, 5, 6]
+console.log("A ", temp);
+
+const myn1 = temp.slice(1,4)
+console.log(myn1);
+console.log("B ", temp);
+
+const myn2 = temp.splice(1,4)
+console.log(myn2);
+console.log("C ", temp);
+
+const mh = [1, 2, 3, 4]
+const dc = [5, 6]
+
+mh.push(dc)
+console.log(mh);
+mh.pop()
+
+const ah = mh.concat(dc)
+console.log(ah);
+
+const ah1 = [...mh, ...dc]
+console.log(ah1);
+
+const num = [1, 2, 3, [5, 6], 7, [8, [9, 3, 5]], 6]
+const num1 = num.flat(Infinity)
+console.log(num1);
+
+console.log(Array.isArray("akshat"));
+console.log(Array.from("akshat"));
+console.log(Array.of("akshat"));
+console.log(Array.of(100, 200, 300));
